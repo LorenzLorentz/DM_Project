962b153346c353253b45a0bda63995cda872fec1
wpj <wpj24@mails.tsinghua.edu.cn>
2025-05-18 22:34:57 +0800

    adaboost added && yet still not work

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+import copy
 
 import numpy as np
 import pandas as pd
@@ -13,6 +14,9 @@ import hydra
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
 
 from model import MLPModel
 from dataset import Dataset
@@ -20,18 +24,19 @@ from utils import print_metrics
 
 class Trainer:
     def __init__(self, criterion:str="BCEWithLogits", device:str=None):
+        self.device = device
         self.model = MLPModel(device=device)
         self.optimizer = optim.Adam(self.model.parameters())
 
         if criterion == "BCEWithLogits":
-            self.criterion = nn.BCEWithLogitsLoss()
+            self.criterion = nn.BCEWithLogitsLoss(torch.Tensor([100.0], device=device))
         else:
             raise NotImplementedError()
 
     def train(self, train_loader:DataLoader, val_loader:DataLoader, load_path:str=None, save_path:str=None, num_epochs:int=None, patience:int=10000):
         self.model.load(path=load_path)
         history = {'train_loss': [], 'val_loss': [], 'val_auc': [], 'val_accuracy': [], 'val_precision': []}
-        best_val_loss = float('inf')
+        best_val_auc = -float('inf')
         epochs_no_improve = 0
 
         epoch_iterator = tqdm(range(num_epochs), desc="Epochs")
@@ -87,11 +92,11 @@ class Trainer:
             
             epoch_iterator.set_postfix(postfix_stats)
 
-            if val_loss < best_val_loss:
-                best_val_loss = val_loss
+            if metrics['auc'] > best_val_auc:
+                best_val_auc = metrics['auc']
                 epochs_no_improve = 0
                 self.model.save(path=save_path)
-                tqdm.write(f"Epoch {epoch+1}: New best model saved with val_loss: {best_val_loss:.4f}") # Use tqdm.write for this
+                tqdm.write(f"Epoch {epoch+1}: New best model saved with val_loss: {best_val_auc:.4f}") # Use tqdm.write for this
             else:
                 epochs_no_improve += 1
                 if epochs_no_improve > patience:
@@ -110,14 +115,37 @@ class Trainer:
                 predictions_prob.extend(torch.sigmoid(outputs).cpu().numpy())
         return np.array(predictions_prob).flatten()
 
+def train_adaboost(model, X:pd.DataFrame, y:pd.Series, skf) -> None:
+    X = X.reset_index(drop=True).values
+    y = y.reset_index(drop=True)
+    auc_scores = []
+
+    for fold, (train_idx, val_idx) in enumerate(skf.split(X, y)):
+        X_train, X_val = X[train_idx], X[val_idx]
+        y_train, y_val = y[train_idx], y[val_idx]
+        model.fit(X_train, y_train)
+        
+        y_prob = model.predict_proba(X_val)[:, 1]
+        binary = (y_prob > 0.5).astype(int)
+        auc = roc_auc_score(y_val, y_prob)
+        print_metrics(y_val, y_prob, binary)
+
+        auc_scores.append(auc)
+        print(f"Fold {fold}: AUC = {auc}")
+
+    print(f"Average AUC: {np.mean(auc_scores)}\n")
+
 @hydra.main(config_name="train", config_path="config", version_base="1.3")
 def train(config):
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     train_df = pd.read_csv(os.path.join(config.data.path, config.data.train_file), index_col='customer_id')
 
-    X = train_df.drop("label", axis=1)
-    y = train_df["label"]
+    X = train_df.drop("label", axis=1).reset_index(drop=True)
+    y = train_df["label"].reset_index(drop=True)
+
+    # train_adaboost(copy.deepcopy(X), copy.deepcopy(y))
+    # return
 
     X = X.replace([np.inf, -np.inf], np.nan)
     for col in X.columns:
@@ -149,7 +177,7 @@ def train(config):
     submission.rename(columns={'customer_id':'customer_id','pred':'result'}, inplace=True)
     submission.loc[submission['result']>0.5, 'result'] = 1
     submission.loc[submission['result']<=0.5, 'result'] = 0
-    submission.to_csv('submission.csv',index=False)
+    submission.to_csv('submission.csv', index=False)
 
 if __name__ == "__main__":
     train()
